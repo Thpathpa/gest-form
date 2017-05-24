65c13ae1ab22f754ccee98527178a84d621e1082
thpathpa <thpathpa@msn.com>
2017-05-24 19:50:38 +0200

    text

--- src/com/gestform/modele/Test.java
+++ src/com/gestform/modele/Test.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
 
 public class Test extends Application {
 
@@ -20,7 +21,32 @@ public class Test extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, 800, 600, Color.LIGHTBLUE);
         primaryStage.setScene(scene);
-
+        
+        Circle cercle = new Circle();
+        cercle.setCenterX(300);
+        cercle.setCenterY(200);
+        cercle.setRadius(100);
+        cercle.setFill(Color.YELLOW);
+        cercle.setStroke(Color.ORANGE);
+        cercle.setStrokeWidth(5);
+        
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(300);
+        rectangle.setY(200);
+        rectangle.setWidth(300);
+        rectangle.setHeight(200);
+        rectangle.setFill(Color.GREEN);
+        rectangle.setStroke(Color.DARKGREEN);
+        rectangle.setStrokeWidth(5);
+        rectangle.setArcHeight(30);
+        rectangle.setArcWidth(30);
+        
+        rectangle.setAccessibleText("Bonjour");
+        rectangle.isResizable();
+        
+        root.getChildren().add(cercle);
+        root.getChildren().add(rectangle);//On ajoute le rectangle après le cercle
         primaryStage.show();
     }
+
 }
